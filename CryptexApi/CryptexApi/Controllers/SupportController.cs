cbacfaf4cd03118b91c56f97434718a7172e51d8
Daniil Khokhulya <khokhulya2006@gmail.com>
2025-05-23 07:42:27 +0200

    Added Support Controller

---
+++ CryptexApi/CryptexApi/Controllers/SupportController.cs
@@ -0,0 +1,74 @@
+﻿using CryptexApi.Services.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CryptexApi.Controllers;
+
+[Authorize(Roles = "Support, Admin")]
+[Route("api/support")]
+[ApiController]
+public class SupportController : ControllerBase
+{
+    private readonly ISupportService _supportService;
+
+    public SupportController(ISupportService supportService)
+    {
+        _supportService = supportService;
+    }
+
+    [HttpPost("take-ticket")]
+    public async Task<IActionResult> TakeTicket([FromQuery] int supportId, [FromQuery] int ticketId)
+    {
+        try
+        {
+            await _supportService.TakeTicket(supportId, ticketId);
+            return Ok("Ticket assigned successfully.");
+        }
+        catch (Exception ex)
+        {
+            return BadRequest($"Failed to assign ticket: {ex.Message}");
+        }
+    }
+
+    [HttpPost("resolve-ticket")]
+    public async Task<IActionResult> ResolveTicket([FromQuery] int supportId, [FromQuery] int ticketId)
+    {
+        try
+        {
+            await _supportService.CloseTicket(ticketId, supportId);
+            return Ok("Ticket resolved successfully.");
+        }
+        catch (Exception ex)
+        {
+            return BadRequest($"Failed to resolve ticket: {ex.Message}");
+        }
+    }
+
+    [HttpGet("current-ticket")]
+    public async Task<IActionResult> GetCurrentTicket([FromQuery] int supportId)
+    {
+        try
+        {
+            var ticket = await _supportService.GetCurrentTicket(supportId);
+            return Ok(ticket);
+        }
+        catch (Exception ex)
+        {
+            return BadRequest($"Failed to get current ticket: {ex.Message}");
+        }
+    }
+
+    [HttpGet("get-open-tickets")]
+    public async Task<IActionResult> GetOpenTickets()
+    {
+        try
+        {
+            var tickets = await _supportService.GetOpenTickets();
+            return Ok(tickets);
+        }
+        catch (Exception ex)
+        {
+            return BadRequest($"Failed to get open tickets: {ex.Message}");
+        }
+    }
+}
--- CryptexApi/CryptexApi/Services/Interfaces/ISupportService.cs
+++ CryptexApi/CryptexApi/Services/Interfaces/ISupportService.cs
@@ -6,5 +6,6 @@ public interface ISupportService
 {
     Task TakeTicket(int supportId, int ticketId);
     Task CloseTicket(int ticketId, int supportId);
+    Task<Ticket> GetCurrentTicket(int supportId);
     Task<List<Ticket>> GetOpenTickets();
 }
