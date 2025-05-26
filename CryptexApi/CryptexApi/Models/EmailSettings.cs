d17cf7593a4763d7ed781e63045655e300e32bb0
Daniil Khokhulya <khokhulya2006@gmail.com>
2025-05-26 05:21:00 +0200

    Add strings for email service

---
+++ CryptexApi/CryptexApi/Models/EmailSettings.cs
@@ -0,0 +1,11 @@
+﻿namespace CryptexApi.Models
+{
+    public class EmailSettings
+    {
+        public string SmtpServer { get; set; }
+        public int SmtpPort { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public bool EnableSsl { get; set; }
+    }
+}
