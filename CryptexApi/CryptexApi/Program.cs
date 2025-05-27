3abc9e8ceb86ec3b8549efb1e2586f33036123a1
Daniil Khokhulya <khokhulya2006@gmail.com>
2025-05-27 05:19:21 +0200

    Added Mastodoon service

--- CryptexApi/CryptexApi/Program.cs
+++ CryptexApi/CryptexApi/Program.cs
@@ -2,8 +2,8 @@ using System.Text;
 using CryptexApi.BackGroundJob;
 using CryptexApi.Context;
 using CryptexApi.Identity.Services;
-using CryptexApi.Models;
 using CryptexApi.Models.Identity;
+using CryptexApi.Models.Options;
 using CryptexApi.Repos;
 using CryptexApi.Repos.Interfaces;
 using CryptexApi.Services;
@@ -102,10 +102,17 @@ namespace CryptexApi
             builder.Services.AddScoped<IWalletService, WalletService>();
             builder.Services.AddScoped<IMarketWalletService, MarketWalletService>();
             builder.Services.AddScoped<IUserService, UserService>();
-            builder.Services.AddScoped<ITokenGeneratingService, TokenGeneratingService>(); builder.Services.Configure<EmailSettings>(
+            builder.Services.AddScoped<ITokenGeneratingService, TokenGeneratingService>(); 
+            builder.Services.Configure<EmailSettings>(
                 builder.Configuration.GetSection("EmailSettings"));
 
             builder.Services.AddScoped<IEmailService, EmailService>();
+
+            builder.Services.Configure<MastodonApiOptions>
+                (builder.Configuration.GetSection("Mastodon"));
+
+            builder.Services.AddScoped<IMastodonService, MastodonService>(); 
+            builder.Services.AddHttpClient<IMastodonService, MastodonService>();
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
